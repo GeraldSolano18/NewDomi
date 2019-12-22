f6b4d0a2779a797afe57229a909a9f0b0512857f
GeraldSolano18 <GeraldSolano18>
2019-12-21 23:45:57 -0600

    eslint

---
+++ src/frontend/views/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>
+                HOLA soy HOMEE
+      </h1>
+
+    </div>
+  );
+
+};
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './frontend/routes/app';
+
+//render recibe el componente y donde se empuja el componente
+ReactDOM.render(<App />, document.getElementById('app'));
