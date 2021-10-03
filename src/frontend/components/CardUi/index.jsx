4b3544f033b02d23f5a3f6cf094479f11c2688b5
Gerald Solano <geraldmacias.gm@gmail.com>
2021-10-03 04:46:10 -0600

    :speech_balloon: adding texts

--- src/frontend/components/CardUi/index.jsx
+++ src/frontend/components/CardUi/index.jsx
@@ -14,6 +14,14 @@ export const CardUi = () => {
           </div>
         </Bounce>
       </div>
+      <div className="title__text  my-3">
+        En Nicaragua la mayor actividad económica se encuentra en el sector
+        informal por lo que el crecimiento de micros, pequeñas y medianas
+        empresas siempre es un punto a favor de la economía nicaragüense. Hoy en
+        día las pymes en Nicaragua tienen que adaptarse a diversos cambios que
+        suelen ser constantes por la globalización siendo estos factores
+        importantes en su desarrollo.
+      </div>
       <div className="CardUi_Container">
         <div className="CardUi">
           <div className="CardUi_Circle">
@@ -21,10 +29,11 @@ export const CardUi = () => {
           </div>
           <div className="CardUi_Content">
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Aspernatur sed at temporibus natus mollitia earum consequatur
-              velit ipsum? Aperiam porro quo sint fugit nemo aliquid unde vero
-              esse inventore ducimus!
+              Desarrollar una empresa dedicada a la creación e implementación de
+              sistemas desarrollados a la medida y servicios de mantenimiento y
+              administración de la infraestructura en el mundo IT, apoyándonos
+              permanentemente en criterios innovadores y tecnologías en
+              evolución.
             </p>
           </div>
         </div>
@@ -34,24 +43,21 @@ export const CardUi = () => {
           </div>
           <div className="CardUi_Content">
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Aspernatur sed at temporibus natus mollitia earum consequatur
-              velit ipsum? Aperiam porro quo sint fugit nemo aliquid unde vero
-              esse inventore ducimus!
+              En un mundo estrechamente interrelacionado por las tecnologías de
+              información, ser líder global en la provisión de soluciones
+              innovadoras de software y servicios de infraestructura en el mundo
+              de IT.
             </p>
           </div>
         </div>
         <div className="CardUi">
           <div className="CardUi_Circle">
-            <h2>Objetivos</h2>
+            <h2>Valores</h2>
           </div>
           <div className="CardUi_Content">
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Aspernatur sed at temporibus natus mollitia earum consequatur
-              velit ipsum? Aperiam porro quo sint fugit nemo aliquid unde vero
-              esse inventore ducimus!
-            </p>
+            <ul>
+              <li> • Responsabilidad.</li>
+            </ul>
           </div>
         </div>
       </div>
