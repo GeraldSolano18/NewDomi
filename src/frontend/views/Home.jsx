b11d42c274aa36121ea51d6bd54542550d2f3fe8
Gerald Solano <geraldmacias.gm@gmail.com>
2021-10-14 22:09:35 -0600

    changes in scroll

--- src/frontend/views/Home.jsx
+++ src/frontend/views/Home.jsx
@@ -15,7 +15,6 @@ import online from "../assets/static/icon_online.svg";
 import bud from "../assets/static/icon_bud.svg";
 import onboarding from "../assets/static/icon_onboarding.svg";
 import api from "../assets/static/icon_api.svg";
-import test from "../assets/static/Social.png";
 import "../assets/styles/App.scss";
 
 const images = [
@@ -49,93 +48,97 @@ const images = [
 ];
 const Home = () => {
   return (
-
-    <div className="mt-3">
-      <Slides interval={8000} images={images} />
-      <img src={banner} className="banner" alt="Banner" />
-
-      <CardUi />
-      <div className="two_ph mt-4 mb-5">
-        <Fade left>
-          <ImpactoCard
-            title="Impacto Social"
-            content="Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías
+    <div className="home-container mt-3">
+      <div className="sectionScroll">
+        <Slides interval={8000} images={images} />
+        <img src={banner} className="banner" alt="Banner" />
+      </div>
+      <div className="sectionScroll">
+        <CardUi />
+      </div>
+      <div className="sectionScroll">
+        <div className="two_ph mt-4 mb-5">
+          <Fade left>
+            <ImpactoCard
+              title="Impacto Social"
+              content="Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías
             Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías
             Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones"
-            claseCss="two_1"
-          />
-        </Fade>
-        <Fade right>
-          <ImpactoCard
-            title="Impacto economico"
-            content="En el contexto que nos encontramos hoy en día muchas empresas han tenido que digitalizar, la digitalización hoy en día ha sido vital para muchas empresas para controlar sus operaciones de una manera centralizada, como también ha sido herramientas para los emprendedores de darse a conocer en el mundo digital con el fin de aumentar sus ventas y abrirse al mundo. Hoy en día existen muchas empresas que suelen brindar servicios integrales pero tras las encuesta que hemos realizado muchas PYMES nos han  manifestado que los costos de dichas soluciones suelen ser muy altas, como también no les brinda la retroalimentación necesaria para tomar decisiones que les ayude a optimizar sus procesos o reducir sus costos con el fin de generarles mayores ingresos."
-            claseCss="two_2"
-          />
-        </Fade>
+              claseCss="two_1"
+            />
+          </Fade>
+          <Fade right>
+            <ImpactoCard
+              title="Impacto economico"
+              content="En el contexto que nos encontramos hoy en día muchas empresas han tenido que digitalizar, la digitalización hoy en día ha sido vital para muchas empresas para controlar sus operaciones de una manera centralizada, como también ha sido herramientas para los emprendedores de darse a conocer en el mundo digital con el fin de aumentar sus ventas y abrirse al mundo. Hoy en día existen muchas empresas que suelen brindar servicios integrales pero tras las encuesta que hemos realizado muchas PYMES nos han  manifestado que los costos de dichas soluciones suelen ser muy altas, como también no les brinda la retroalimentación necesaria para tomar decisiones que les ayude a optimizar sus procesos o reducir sus costos con el fin de generarles mayores ingresos."
+              claseCss="two_2"
+            />
+          </Fade>
+        </div>
       </div>
-      <div>
-        <img src={test} alt="" />
-      </div>
-      <div className="why">
-        <div className="wrapper">
-          <Bounce>
-            <h1 className="title__h1">¿Por qué elegirnos a nosotros?</h1>
-          </Bounce>
+      <div className="sectionSCroll">
+        <div className="why">
+          <div className="wrapper">
+            <Bounce>
+              <h1 className="title__h1">¿Por qué elegirnos a nosotros?</h1>
+            </Bounce>
 
-          <div className="why__grid">
-            <Zoom>
-              <div className="why_item">
-                <img src={api} alt="" />
-                <h1 className="title__h2">Online Shop</h1>
-                <p className="copy">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas
-                  laboriosam culpa ipsam sapiente
-                </p>
-              </div>
-            </Zoom>
-            <Zoom>
-              <div className="why_item">
-                <img src={online} alt="" />
-                <h1 className="title__h2">Online Shop</h1>
-                <p className="copy">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas
-                  laboriosam culpa ipsam sapiente
-                </p>
-              </div>
-            </Zoom>
-            <Zoom>
-              <div className="why_item">
-                <img src={bud} alt="" />
-                <h1 className="title__h2">Online Shop</h1>
-                <p className="copy">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas
-                  laboriosam culpa ipsam sapiente, facere officia
-                </p>
-              </div>
-            </Zoom>
-            <Zoom>
-              <div className="why_item">
-                <img src={onboarding} alt="" />
-                <h1 className="title__h2">Online Shop</h1>
-                <p className="copy">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas
-                  laboriosam culpa ipsam sapiente, facere officia
-                </p>
-              </div>
-            </Zoom>
+            <div className="why__grid">
+              <Zoom>
+                <div className="why_item">
+                  <img src={api} alt="" />
+                  <h1 className="title__h2">Online Shop</h1>
+                  <p className="copy">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quas laboriosam culpa ipsam sapiente
+                  </p>
+                </div>
+              </Zoom>
+              <Zoom>
+                <div className="why_item">
+                  <img src={online} alt="" />
+                  <h1 className="title__h2">Online Shop</h1>
+                  <p className="copy">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quas laboriosam culpa ipsam sapiente
+                  </p>
+                </div>
+              </Zoom>
+              <Zoom>
+                <div className="why_item">
+                  <img src={bud} alt="" />
+                  <h1 className="title__h2">Online Shop</h1>
+                  <p className="copy">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quas laboriosam culpa ipsam sapiente, facere officia
+                  </p>
+                </div>
+              </Zoom>
+              <Zoom>
+                <div className="why_item">
+                  <img src={onboarding} alt="" />
+                  <h1 className="title__h2">Online Shop</h1>
+                  <p className="copy">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quas laboriosam culpa ipsam sapiente, facere officia
+                  </p>
+                </div>
+              </Zoom>
+            </div>
           </div>
         </div>
       </div>
+      <div className="sectionScroll">
+        <Fade>
+          <BannerHero />
+        </Fade>
+        {/* <CardHover /> */}
+        <img src={Emprendedores} className="banner" alt="Banner" />
 
-      <Fade>
-        <BannerHero />
-      </Fade>
-      {/* <CardHover /> */}
-      <img src={Emprendedores} className="banner" alt="Banner" />
-
-      <Fade>
-        <Footer />
-      </Fade>
+        <Fade>
+          <Footer />
+        </Fade>
+      </div>
     </div>
   );
 };
